0a9604deed7351d54413bc3504e0f9526834cd85
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-09-07 21:54:24 +0200

    laget oppg2

---
+++ Oppgave3/Oppgave2/src/ingen_hjelp.js
@@ -0,0 +1,14 @@
+import './styles.css';
+import { users } from './util';
+
+// TODO: Hent HTML #id med getElementById
+    const searchInput = document.getElementById('name');
+    const filterInput = document.getElementById('age');
+    const filterButton = document.getElementById('filter');
+    const userUl = document.getElementById('users');
+
+// TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
+// TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
+// TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
+// TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
+// TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
